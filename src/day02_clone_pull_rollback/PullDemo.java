4ebf3be8b071b06abff810ba40c8a55c2404b424
Tedbiryay <90892978+Tedbiryay@users.noreply.github.com>
2025-03-16 14:34:53 +0000

    Update PullDemo.java
    Helle My Cydeo Friends

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -4,5 +4,7 @@ public class PullDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello From Local");
+
+        System.out.println("Hello From teammate who pushed to github");
     }
 }
